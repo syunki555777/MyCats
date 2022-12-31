57a4655b798d605e755d44f567e849e045c2fb2f
shungiku <syunki555777@gmail.com>
2023-01-01 01:52:58 +0900

    最終更新(公開前)

--- script.js
+++ script.js
@@ -1,14 +1,123 @@
-$(function (){
+
+function onLoad() {
+
+    var loc = location.href;
+    history.replaceState({"pageURL": loc}, null, loc);
+    console.log(loc);
+
+    $(".photo-view").hide();
+
     $(".menu-background").hide();
 
-    $(".menu-button").on("click",function(){
-        $(".menu-background").slideToggle();
+    $(".menu-button").on("click", function () {
+        $(".menu-background").slideDown();
     });
-    $("#close").on("click",function(){
-        $(".menu-background").slideToggle();
+    $("#close").on("click", function () {
+        $(".menu-background").slideUp();
     });
 
 
-});
+    $(".member-button").on("click", function () {
+        URLLoadContainer("/member.html", "メンバー");
+        stateUpdate("member.html");
+    });
+
+    $(".story-button").on("click", function () {
+        URLLoadContainer("/story.html", "ストーリー");
+        stateUpdate("story.html");
+    });
+
+    $(".about-button").on("click", function () {
+        URLLoadContainer("/about.html", "アバウト");
+        stateUpdate("about.html");
+    });
+
+    $(".photo-button").on("click", function () {
+        URLLoadContainer("/photo.html", "写真");
+        stateUpdate("photo.html");
+    });
+
+    $(window).on("popstate", function (e) {
+        var state = e.originalEvent.state;
+
+        if (state) {
+            var url = state.pageURL;
+
+            URLLoadContainer(url, null);
+
+        }
+    });
+
+    $("a").on("click", function (e) {
+        e.preventDefault();
+        var link = $(this).attr("href");
+        console.log(link);
+        URLLoadContainer(link, link);
+        stateUpdate(link);
+        $(".menu-background").slideUp();
+    });
+
+    $(".photo-view img").on("click", function () {
+        $(".photo-view").fadeOut();
+    });
+
+    $(".photo-box img").on("click", function () {
+        var image = $(this).attr("src");
+        console.log(image + "画像がクリックされました");
+        var replaceImage = image.replace("small", "large");
+        $(".photo-view img").attr("src", replaceImage);
+        $(".photo-view").fadeIn();
+    });
+};
+
+window.addEventListener('DOMContentLoaded', onLoad);
 
 
+function URLLoadContainer(URL,name){
+    const container = $(".container");
+
+    container.animate({
+        opacity:0
+    },500,function (){
+        container.empty();
+        $.ajax({
+            beforeSend:function (jqXHR, settings) {
+                jqXHR.overrideMimeType("text/html;charset=UTF-8");
+            },
+            url: URL,
+            success:function (data){
+                container.text("読み込み成功");
+                var loadHTML = $(data).find(".container");
+
+                console.log( loadHTML);
+                // $(data).find("div").each(function (){
+                //     if($(this).is(".container")){
+                //         container.html($(this).text());
+                //         console.log($(this).text());
+                //     }
+                // });
+
+                container.html(loadHTML);
+
+                container.animate({
+                    opacity:1
+                },500);
+
+                $("*").off();
+
+                onLoad();
+            },
+            error:function (data){
+                container.text("読み込みに失敗しました。");
+            }
+
+        })
+
+
+    });
+}
+function stateUpdate(URL){
+    var state = {"pageURL": URL};
+
+    history.pushState(state,null, URL);
+}
